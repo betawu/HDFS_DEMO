f5d9905e2e42779e501f839894157d77d5b16f91
beta <541737447@qq.com>
2019-02-07 13:37:02 +0800

    util

--- src/main/java/com/beta/server/hadoop/mr/project/utils/LogParser.java
+++ src/main/java/com/beta/server/hadoop/mr/project/utils/LogParser.java
@@ -1,7 +1,25 @@
 package com.beta.server.hadoop.mr.project.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by beta on 2019/2/7.
  */
 public class LogParser {
+    public Map<String, String> parse(String log) {
+        String[] arr = log.split("\001");
+
+        String ip = arr[13];
+        Map<String, String> map = new HashMap<>();
+
+        IPParser.RegionInfo info = IPParser.getInstance().analyseIp(ip);
+
+        map.put("ip",ip);
+        map.put("country", info.getCountry() == null ? "-" : info.getCountry());
+        map.put("province", info.getProvince() == null ? "-" : info.getProvince());
+        map.put("city", info.getCity() == null ? "-" : info.getCity());
+
+        return map;
+    }
 }
