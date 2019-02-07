5469093dc1a1423f146f55365deb8b6b492ac67f
beta <541737447@qq.com>
2019-02-07 13:08:19 +0800

    project

---
+++ src/main/java/com/beta/server/hadoop/mr/project/mr/PVStatAPP.java
@@ -0,0 +1,7 @@
+package com.beta.server.hadoop.mr.project.mr;
+
+/**
+ * Created by beta on 2019/2/6.
+ */
+public class PVStatAPP {
+}
---
+++ src/main/java/com/beta/server/hadoop/mr/project/utils/IPSeeker.java
@@ -0,0 +1,7 @@
+package com.beta.server.hadoop.mr.project.utils;
+
+/**
+ * Created by beta on 2019/2/7.
+ */
+public class IPSeeker {
+}
